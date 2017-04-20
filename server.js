92931e323cf25b04963db8fc0eb081b1a3680f93
589865 <stanleygorman@C02T44V8H03Q.home>
2017-04-20 10:40:07 -0400

    heroku not working

--- server.js
+++ server.js
@@ -9,24 +9,24 @@ var io = require('socket.io')(http);
 var path = require('path');
 
 // Initialize appication with route / (that means root of the application)
-// app.get('/chat', function(req, res){
-//   var express = require('express');
-//   app.use(express.static(path.join(__dirname)));
-//   res.render("new.ejs");
-// });
-//
-//
-// //http://javabeginnerstutorial.com/javascript-2/create-simple-chat-application-using-node-js-express-js-socket-io/\
-// //THIS IS NOT MY A CODE
-// // Register events on socket connection
-// io.on('connection', function(socket){
-//   socket.on('chatMessage', function(from, msg){
-//     io.emit('chatMessage', from, msg);
-//   });
-//   socket.on('notifyUser', function(user){
-//     io.emit('notifyUser', user);
-//   });
-// });
+app.get('/chat', function(req, res){
+  var express = require('express');
+  app.use(express.static(path.join(__dirname)));
+  res.render("new.ejs");
+});
+
+
+//http://javabeginnerstutorial.com/javascript-2/create-simple-chat-application-using-node-js-express-js-socket-io/\
+//THIS IS NOT MY A CODE
+// Register events on socket connection
+io.on('connection', function(socket){
+  socket.on('chatMessage', function(from, msg){
+    io.emit('chatMessage', from, msg);
+  });
+  socket.on('notifyUser', function(user){
+    io.emit('notifyUser', user);
+  });
+});
 
 
 
@@ -85,12 +85,12 @@ app.get('/', function(req, res){
   });
 
 });
-
-app.listen(port, function(){
-  console.log('listening');
-});
-
-// // Listen application request on port 3000
-// http.listen(3000, function(){
-//   console.log('listening on :3000');
+//
+// app.listen(port, function(){
+//   console.log('listening');
 // });
+
+// Listen application request on port 3000
+http.listen(3000, function(){
+  console.log('listening on :3000');
+});
